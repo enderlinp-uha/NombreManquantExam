3f1ae2b7adf1bd3457b5932256a5457f7f7bb2b0
enderlinp-uha <philippe.enderlin@uha.fr>
2025-02-03 09:21:59 +0100

    UPDATE

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] numbers = { 1, 2, 4, 5, 6 };
+        System.out.println("Entrée : " + Arrays.toString(numbers));
+        System.out.println("Sortie : " + findMissingNumber(numbers));
+
+        System.out.println();
+
+        int[] numbers2 = { 2, 3, 1, 5 };
+        System.out.println("Entrée : " + Arrays.toString(numbers2));
+        System.out.println("Sortie : " + findMissingNumber(numbers2));
+    }
+
+    public static int findMissingNumber(int[] numbers) {
+        Arrays.sort(numbers);
+        int length = numbers.length;
+        for (int i = 0; i < length; i++) {
+            int nextIndex = i + 1;
+            if (numbers[i] > nextIndex) {
+                return nextIndex;
+            }
+        }
+        return -1;
     }
 }
