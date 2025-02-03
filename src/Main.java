097c15cb0d97d39c10da8f72aa7e135a6e9a878c
enderlinp-uha <philippe.enderlin@uha.fr>
2025-02-03 12:56:20 +0100

    Suppresion de la variable nextIndex et gestion de l'erreur Index out of bounds

--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,25 @@ public class Main {
     public static void main(String[] args) {
         int[] numbers = { 1, 2, 4, 5, 6 };
         System.out.println("Entrée : " + Arrays.toString(numbers));
-        System.out.println("Sortie : " + findMissingNumber(numbers));
-
-        System.out.println();
+        System.out.println("Sortie : " + findMissingNumber(numbers) + "\n");
 
         int[] numbers2 = { 2, 3, 1, 5 };
         System.out.println("Entrée : " + Arrays.toString(numbers2));
-        System.out.println("Sortie : " + findMissingNumber(numbers2));
+        System.out.println("Sortie : " + findMissingNumber(numbers2) + "\n");
+
+        int[] numbers3 = { 1, 2, 3, 4, 5 };
+        System.out.println("Entrée : " + Arrays.toString(numbers3));
+        System.out.println("Sortie : " + findMissingNumber(numbers3));
     }
 
     public static int findMissingNumber(int[] numbers) {
         Arrays.sort(numbers);
         int length = numbers.length;
         for (int i = 0; i < length; i++) {
-            int nextIndex = i + 1;
-            if (numbers[i] > nextIndex) {
-                return nextIndex;
+            if (i < length - 1) {
+                if (numbers[i + 1] - numbers[i] > 1) {
+                    return numbers[i] + 1;
+                }
             }
         }
         return -1;
