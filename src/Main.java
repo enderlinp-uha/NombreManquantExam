37dc484930f68fef75d1e9ae375a68b58b92ae6e
enderlinp-uha <philippe.enderlin@uha.fr>
2025-02-04 10:35:36 +0100

    ADD comments

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = { 1, 2, 4, 5, 6 };
+        int[] numbers = { 1, 2, 4, 5, 6 }; // 3 is missing
         System.out.println("Entrée : " + Arrays.toString(numbers));
         System.out.println("Sortie : " + findMissingNumber(numbers) + "\n");
 
-        int[] numbers2 = { 2, 3, 1, 5 };
+        int[] numbers2 = { 2, 3, 1, 5 }; // 4 is missing
         System.out.println("Entrée : " + Arrays.toString(numbers2));
         System.out.println("Sortie : " + findMissingNumber(numbers2) + "\n");
 
-        int[] numbers3 = { 1, 2, 3, 4, 5 };
+        int[] numbers3 = { 1, 2, 3, 4, 5 }; // -1
         System.out.println("Entrée : " + Arrays.toString(numbers3));
         System.out.println("Sortie : " + findMissingNumber(numbers3));
     }
